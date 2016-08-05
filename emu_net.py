fdf06415e7cae1dc99a355f4acc57ac03653d436
superorc <rykanoga@gmail.com>
2016-08-06 00:38:48 +0300

    unzip to f_id

--- emu_net.py
+++ emu_net.py
@@ -2,11 +2,19 @@
 #http://www.emu-land.net/consoles/genesis/roms?act=getfile&id=1
 # id 1034 is last, Zoop game
 
-import requests, libarchive.public
 
-out = open('./%id.7zip', 'w') , %id
+import requests, subprocess
+
+out = open('./id.7zip', 'w')
 url = "http://www.emu-land.net/consoles/genesis/roms?act=getfile&id=1034"
 r = requests.get(url)
 
 #save archive
 out.write (r.content)
+out.close()
+
+#unpack
+#subprocess
+subprocess.call("mkdir -p f_id", shell=True)
+subprocess.call("7z e id.7zip -of_id *[!].gen -y", shell=True)
+# -oc:t/ -r"])
