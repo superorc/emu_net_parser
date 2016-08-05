1a5374092c1420134e0e08b3488dc34dc037c570
superorc <rykanoga@gmail.com>
2016-08-06 01:49:00 +0300

    works with rom_id

--- emu_net.py
+++ emu_net.py
@@ -3,18 +3,24 @@
 # id 1034 is last, Zoop game
 
 
-import requests, subprocess
+import requests, subprocess, os
 
-out = open('./id.7zip', 'w')
-url = "http://www.emu-land.net/consoles/genesis/roms?act=getfile&id=1034"
+rom_id = 1034
+url = "http://www.emu-land.net/consoles/genesis/roms?act=getfile&id=%s" % rom_id
 r = requests.get(url)
+
+out_7zip = "%s.7zip" % rom_id
+out = open(out_7zip, 'w')
 
 #save archive
 out.write (r.content)
 out.close()
 
-#unpack
-#subprocess
-subprocess.call("mkdir -p f_id", shell=True)
-subprocess.call("7z e id.7zip -of_id *[!].gen -y", shell=True)
-# -oc:t/ -r"])
+#mkdir
+dir_name = str(rom_id)
+if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
+
+#extract 7zip
+cmd = ['7z', 'e', out_7zip, '-o' + dir_name + 'y']
+subprocess.call(cmd)
