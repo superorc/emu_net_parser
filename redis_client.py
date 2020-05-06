2e15f31d9abdb50262fdca65681c7f1b6c789931
superorc <rykanoga@gmail.com>
2020-05-06 20:57:30 +0300

    tmp redis client

--- redis_client.py
+++ redis_client.py
@@ -1,14 +1,30 @@
+import asyncio
 import aioredis
 
 
 class RedisClient:
 
-    def __init__():
+    def pub_to_redis(self, file_name):
+        pass
+
+    def get_from_redis(self, file_name):
+        pass
+
+    async def main():
         """ Create redis connection """
-        pass
+        try:
+            redis = await aioredis.create_redis_pool('redis://localhost')
+        except:
+            print("Connection to redis failed")
+            raise
+        
+        await redis.set('my-key', 'value')
 
-    def pub_to_redis(file_name):
-        pass
+        value = await redis.get('my-key', encoding='utf-8')
+        print(value)
 
-    def get_from_redis(file_name):
-        pass
+        redis.close()
+        await redis.wait_closed()
+
+
+    asyncio.run(main())
